8e329efb01d5d4a3d7e176f9ccb8f303c30f3539
Shivanand Shegunasi <shegunasishivanand@gmail.com>
2022-10-18 22:11:06 +0530

    Algorithm-Visualizer-master is created

---
+++ Merge_Sort.js
@@ -0,0 +1,108 @@
+var from,mid,to;
+var mi,mj,mk;
+var global_merge1_interval;
+var temp; 
+var flag=0;
+
+function merge()
+{
+    
+    var elements = document.getElementById('array').children; 
+    var n=elements.length; 
+    while (mi <= mid && mj <= to)
+    {
+        var element1=elements[mi].style;
+        var element2=elements[mj].style;
+        var val1=parseInt(element1.height);
+        var val2=parseInt(element2.height);
+        
+        // console.log("Val1",val1,val2);
+        if (val1 < val2)
+        {
+            // console.log("Val1",val1,val2);
+            temp[mk++] = val1;
+            mi++;
+        }
+        else
+            {
+                temp[mk++] = val2;
+                mj++;
+            }
+    }
+ 
+    while (mi < n && mi <= mid)
+    {
+        var element1=elements[mi].style;  
+        var val1=parseInt(element1.height);
+        temp[mk++] = val1;
+        mi++;
+    }
+    
+ 
+    // console.log(temp);
+    
+    for (mi = from; mi <= to; mi++)
+    {   
+        var element1=elements[mi].style;
+        element1.height=temp[mi]+"px";
+        // element1.backgroundColor ="aquamarine";
+        if(flag==0)
+        {
+            element1.backgroundColor ="red";
+        }
+        else{
+            element1.backgroundColor ="orange";
+        }
+    }
+    if(flag==0)
+    {
+        flag=1;
+    }
+    else{
+        flag=0;
+    }
+}
+
+
+function mergeSort()  
+{  
+    
+    var elements = document.getElementById('array').children; 
+    var n=elements.length; 
+
+    from = i;
+    mid = i + m - 1;
+    to = Math.min(i + 2*m - 1, n-1);
+     
+    mk = from;
+    mi = from; 
+    mj = mid + 1;
+    // console.log(from,mid,to);
+    // console.log(mk,mi,mj);
+    merge();
+
+    i+=(2*m);
+    if(i>=(n-1))
+    {
+        m=m*2;
+        i=0;
+    }
+    if(m>=n)
+    {
+        end = window.performance.now();
+        console.log(`Execution time: ${end - start} ms`);
+        clearInterval(globinterval);
+    }
+}  
+
+function merge_sortonclick()
+{
+    start = window.performance.now();
+    i=0,m=1;
+    var elements = document.getElementById('array').children; 
+    var n=elements.length; 
+    var myArray = new Array(n);
+    temp=myArray;
+    var startinterval=setInterval(mergeSort, 1000);
+    globinterval=startinterval;
+}
---
+++ script.js
@@ -0,0 +1,66 @@
+var generate_array = document.getElementById("array_generator");
+var array_size_input = document.getElementById("arraySizeInput");
+var random_array = document.getElementById("radom_array");
+var array_generated = document.getElementById("array");
+
+var i = 0,
+  j = 0,
+  m = 0;
+var start, end; //for recording the time
+var globinterval;
+
+function random_array_generator() {
+  array_generated.innerHTML = "";
+  let n = randomIntFromInterval(10, 100);
+  for (var k = 0; k < n; k++) {
+    var val = randomIntFromInterval(10, 100);
+    var node = document.createElement("div");
+    node.setAttribute("class", "bar_view");
+    node.style.height = val * 4 + "px";
+    // var textnode = document.createTextNode(val);
+    // node.appendChild(textnode);
+    array_generated.appendChild(node);
+  }
+  i = 0;
+  j = 0;
+}
+
+function array_generator_random() {
+  let array_size = array_size_input.value || 10;
+  array_generated.innerHTML = "";
+  for (var k = 0; k < array_size; k++) {
+    // var val = randomIntFromInterval(10, 100);
+    var val = randomIntFromInterval(10, 100);
+    var node = document.createElement("div");
+    node.setAttribute("class", "bar_view");
+    node.style.height = val * 4 + "px";
+    // var textnode = document.createTextNode(val);
+    // node.appendChild(textnode);
+    array_generated.appendChild(node);
+  }
+  i = 0;
+  j = 0;
+}
+
+function array_generator_elements() {
+  let arrayValues = document.getElementById("arrvalues").value;
+  let splittedValues = arrayValues.split(" ");
+  let array_size = splittedValues.length || 10;
+  array_generated.innerHTML = "";
+  for (var k = 0; k < array_size; k++) {
+    // var val = randomIntFromInterval(10, 100);
+    var val = splittedValues[k] * 5;
+    var node = document.createElement("div");
+    node.setAttribute("class", "bar_view");
+    node.style.height = val * 4 + "px";
+    // var textnode = document.createTextNode(val);
+    // node.appendChild(textnode);
+    array_generated.appendChild(node);
+  }
+  i = 0;
+  j = 0;
+}
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
